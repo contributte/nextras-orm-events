dbe22caa1f7c75c641f2185a05ab8a11c91d73af
Marek Bartoš <bartos.developer152@gmail.com>
2019-11-18 01:48:31 +0100

    Traits support

---
+++ tests/fixtures/Mocks/Foo/FooTraitListener.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Fixtures\Mocks\Foo;
+
+use Contributte\Nextras\Orm\Events\Listeners\BeforePersistListener;
+use Nextras\Orm\Entity\IEntity;
+
+final class FooTraitListener implements BeforePersistListener
+{
+
+	/** @var callable[] */
+	public $onCall = [];
+
+	/** @var string[] */
+	public $onCallHistory = [];
+
+	public function onBeforePersist(IEntity $entity): void
+	{
+		$method = str_replace(self::class . '::', null, __METHOD__);
+		foreach ($this->onCall as $cb) {
+			$cb($method, $entity);
+		}
+		$this->onCallHistory[] = $method;
+	}
+
+}
